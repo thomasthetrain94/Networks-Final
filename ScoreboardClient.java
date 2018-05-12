20a2e504906789b38e2720d895f3d1d37057c538
thomasthetrain94 <36656088+thomasthetrain94@users.noreply.github.com>
2018-05-11 20:08:02 -0400

    Update ScoreboardClient.java
    Updated code from gitreadyforthis

--- ScoreboardClient.java
+++ ScoreboardClient.java
@@ -1,110 +1,100 @@
-import java.io.IOException;
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.IOException;
 import java.io.PrintWriter;
-import java.net.Socket;
 import java.util.ArrayList;
-import java.util.Random;
 
 /**
  * Collect all the Scoreboard Client classes in this file. Note that this file
  * MUST BE called ScoreboardClient.java
  */
-public class ScoreboardClient implements Runnable {    
-    protected BufferedReader input;
-    protected PrintWriter output;
+public class ScoreboardClient implements Runnable {
+    private String gameChoice;
+    private BufferedReader input;
+    private PrintWriter output;
     private ScoreboardClient master;
     //Interact with the questions from the challenge Response
     private ArrayList<ChallengeResponseGame> games;
-    protected void registerCallback(ScoreboardClient c) {
-        this.master = c;
-    }
-    public ScoreboardClient(BufferedReader input, PrintWriter output) {
+
+    ScoreboardClient(BufferedReader input, PrintWriter output, ArrayList<ChallengeResponseGame> games) {
         this.input = input;
         this.output = output;
+        this.games = games;
     }
-    public void run(){
-        String player = "";
+
+    void registerCallback(ScoreboardClient c) {
+        this.master = c;
+    }
+
+    public void run() {
         int port = 4001;
-        try{
+        try {
+
             //Creates a server socket, bound to the specified port.
             //should connect to the localhost on port 4001
-            Socket clientSocket = new Socket("localhost",port); 
-            //Output and Input stream creation
-            //Output sends data to the server
-            PrintWriter output = new PrintWriter(clientSocket.getOutputStream(),true);
-            //Input Recieves data from the server
-            BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            System.out.println("User Name Selected");
+            this.master.output.println("User Name Selected");
             //Username Declariation
-            String UserName = input.readLine();
+            String UserName = this.master.input.readLine();
             //Add to array of people
-            System.out.println("User Name Selected");
-            while(true){                
-                String userInput = input.readLine();
-                switch(userInput){
+            this.master.output.println("User Name Selected");
+            while (true) {
+                String userInput = this.master.input.readLine().toLowerCase();
+                switch (userInput) {
                     //Or this may have to be done with an IF Else statements which might
                     //be a cleaner solution
                     //I moved most of this code to Client since it is interactive while
                     //Server is not
-                    case "Help": System.out.println("Other Commands");
-                    System.out.println("Expected Inputs: Help, Game, Question, Scoreboard, Random");
-                    break;
-                    case "Game": System.out.println("Choose game Crypto or Networking");
-                    //The available games
-                    System.out.println("0:Crypto /n1:Networking");
-                    //Choosing the which game to play
-                    String gameChoice = input.readLine();
-                    //Converts the input to the index of game where
-                    int apple=games.indexOf(gameChoice);
-                    //the currentGame is created with the games array at that array
-                    ChallengeResponseGame currentGame=games.get(apple);                    
-                    //The available Questions
-                    System.out.println("Question 1 or Question 2");
-                    //Choosing the which question to play                    
-                    String gameChoice2 = input.readLine();
-                    //Converts the input to the index of game where
-                    int banana=games.indexOf(gameChoice2);
-                    //The question is choosen
-                    String question = currentGame.getQuestions().get(banana).getQuestion();
-                    //UserAnswer for the question
-                    String userAnswer = input.readLine();
-                    //checking the answer for the question of the current game by this.UserName
-                    currentGame.answer(UserName, question, userAnswer);
-                    break;
-                    case "ScoreBoard": System.out.println("Print Scoredboard");
-                    String gameChoice3 = input.readLine();
-                    //Converts the input to the index of game where
-                    int canalope=games.indexOf(gameChoice3);
-                    //Should get the game at the choice of the user and return the names and scores
-                    games.get(canalope).getScores();
-                    break;
-                    case "Random": System.out.println("Choosing Random Game");
-                    //Random number creation for games
-                    Random generator = new Random();   
-                    //Apply the number to the list of games
-                    ChallengeResponseGame currentGameRandom=games.get(generator.nextInt(1));
-                    //add the username to the current game
-                    currentGameRandom.addPlayer(UserName);                
-                    //Question selection is done by random choice
-                    //Store the question for the current game by getting the questions
-                    //then index of the next random number to get the single question
-                    String questionRandom = currentGameRandom.getQuestions().get(generator.nextInt(1)).getQuestion();
-                    //UserAnswer for the question
-                    System.out.println(questionRandom);
-                    String userAnswerRandom = input.readLine();
-                    //checking the answer for the question of the current game by this.UserName
-                    System.out.println(currentGameRandom.answer(UserName, questionRandom, userAnswerRandom));
-                    break;
-                    default: System.out.println("Unexpected Input");
-                } 
-            }    
-            }catch(IOException ex) {
-            System.err.println(String.format("Unable to connect to port %d",port));
-            }catch(java.lang.ArrayIndexOutOfBoundsException ex){
-               System.err.println("Shit went to far");
-            }catch(java.lang.NullPointerException ex){
-               System.err.println("UserInput for switch")  ;
+                    case "help":
+                        this.master.output.println("Other Commands");
+                        this.master.output.println("Expected Inputs: Help, Game, Scoreboard");
+                        break;
+                    case "game":
+                        this.master.output.println("Choose game Crypto or Networking");
+                        //The available games
+                        this.master.output.println("crypto \n networking");
+                        //Choosing the which game to play
+                        gameChoice = this.master.input.readLine();
+                        //the currentGame is created with the games array at that array
+                        ChallengeResponseGame currentGame = null;
+                        for (ChallengeResponseGame game : this.games) {
+                            if (game.gameId.equals(gameChoice)) {
+                                currentGame = game;
+                            }
+                        }
+                        //The available Questions
+                        this.master.output.println("Question 1 or Question 2");
+                        //Choosing the which question to play
+                        int qChoice = Integer.parseInt(this.master.input.readLine());
+                        //The question is choosen
+                        String question = null;
+                        if (currentGame != null) {
+                            question = currentGame.getQuestions().get(qChoice).getQuestion();
+                            this.master.output.println(question);
+                        }
+                        //UserAnswer for the question
+                        String userAnswer = this.master.input.readLine();
+                        //checking the answer for the question of the current game by this.UserName
+                        if (currentGame != null) {
+                            currentGame.answer(UserName, question, userAnswer);
+                        }
+                        break;
+                    case "scoreboard":
+                        this.master.output.println("Print ScareBird");
+
+                        //Converts the input to the index of game where
+                        int gameScareBird = games.indexOf(gameChoice);
+                        //Should get the game at the choice of the user and return the names and scores
+                        games.get(gameScareBird).getScores();
+                        break;
+                    default:
+                        this.master.output.println("Unexpected Input Try Again!");
+                }
+            }
+        } catch (IOException ex) {
+            System.err.println(String.format("Unable to connect to port %d", port));
+        } catch (java.lang.ArrayIndexOutOfBoundsException ex) {
+            System.err.println("Shit went to far");
+        } catch (java.lang.NullPointerException ex) {
+            System.err.println("Noll Ponter");
         }
     }
 }
