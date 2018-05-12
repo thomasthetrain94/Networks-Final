705f6ad496149bad23bd75a8a1fc805a9a0057ce
thomasthetrain94 <36656088+thomasthetrain94@users.noreply.github.com>
2018-05-11 20:08:38 -0400

    Update ScoreboardServer.java

--- ScoreboardServer.java
+++ ScoreboardServer.java
@@ -1,19 +1,14 @@
-import java.io.IOException;
 import java.io.BufferedReader;
-import java.io.PrintWriter;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.InetAddress;
 import java.util.ArrayList;
-import java.util.Random; 
-import java.lang.Error;
-
 
 
 //Must support mutlple clients at the same time  Some version of a Player Handler
-//Questions are only specify to a game: Keep track of players  Player handler 
+//Questions are only specify to a game: Keep track of players  Player handler
 //Authenticate players by name
 //PLayer handler is how the player(Client) communicates with the server and the server communicates with the player(Client)
 /**
@@ -21,41 +16,61 @@ import java.lang.Error;
  * MUST BE called ScoreboardServer.java
  */
 //middle man between client and game
-public class ScoreboardServer{
-    private ArrayList<ChallengeResponseGame> games;   
-    private ArrayList<ScoreboardClient> Players; 
+class ScoreboardServer{
+    private ArrayList<ChallengeResponseGame> games;
+    private ArrayList<ScoreboardClient> Players;
+
     public void startServer(int sslPort){
-        try{
-            //Creates a server socket, bound to the specified port.
-            ServerSocket server = new ServerSocket(sslPort);
-            //listens for activity on the server
-            while (true) {  
-                Socket client = server.accept();
-                BufferedReader input = new BufferedReader(new InputStreamReader(client.getInputStream())); 
-                PrintWriter output = new PrintWriter(client.getOutputStream(),true);                
-                // WRITE ME: accept connection, extract streams and start thread                
-                ScoreboardClient clientThread = new ScoreboardClient(input, output);
-                Thread t = new Thread(clientThread);
-                t.start();//memory error    
-                // register callback
-                clientThread.registerCallback(clientThread);
-                Players.add(clientThread);
-            }            
-        }catch (IOException ex) {
-            System.err.println(String.format("Unable to connect to port %d", 
-                    sslPort));
-        } catch(java.lang.ArrayIndexOutOfBoundsException ex){
-            System.err.println("Shit went to far");
-        }catch(java.lang.NullPointerException ex){
-            System.err.println("UserInput for switch")  ;
-        }/*catch(java.lang.OutofMemoryError ex){
-            System.err.println("I can't remember");
-        }*/
-    }        
+
+        //Creates a server socket, bound to the specified port.
+        ServerSocket server = null;
+        try {
+            server = new ServerSocket(sslPort);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //listens for activity on the server
+        while (true) {
+            Socket client = null;
+            try {
+                if (server != null) {
+                    client = server.accept();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            BufferedReader input = null;
+            try {
+                if (client != null) {
+                    input = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            PrintWriter output = null;
+            try {
+                if (client != null) {
+                    output = new PrintWriter(client.getOutputStream(),true);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            // WRITE ME: accept connection, extract streams and start thread
+            ScoreboardClient clientThread = new ScoreboardClient(input, output, games);
+            Thread t = new Thread(clientThread);
+            t.start();//memory error
+            // register callback
+            clientThread.registerCallback(clientThread);
+
+        }
+
+    }
+
     //Arraylist to hold the list of ChallengeResponseGame
-    public ScoreboardServer(ArrayList<ChallengeResponseGame> games){ 
+    ScoreboardServer(ArrayList<ChallengeResponseGame> games){
         this.games = games;
     }
+
     //setup for the ssl Connection
     //This will use the SSL with keystore and pasword
     //This will be ssl
@@ -65,13 +80,13 @@ public class ScoreboardServer{
     //Start the server itself
 }
 /*
- ***ERRORS***  
+ ***ERRORS***
 User Name Selected
 Print Scoredboard
 User Name Selected
 Choosing Random Game
 What block cipher uses a block size of 128 bits?
-User Name Selected 
+User Name Selected
  ***User Name is not being Saved Assume it is just a matter of program going over again***
 User Name Selected
 Choosing Random Game
@@ -90,14 +105,14 @@ Choose game Crypto or Networking
 0:Crypto /n1:Networking
 void Help(){//temp methods just for ease of understanding
 System.out.println("Expected Inputs: Help, Game, Question, Scoreboard, Random");
-}        
-void randomGame(){ //temp methods just for ease of understanding           
+}
+void randomGame(){ //temp methods just for ease of understanding
 //Random number creation for games
-Random generator = new Random();   
+Random generator = new Random();
 //Apply the number to the list of games
 ChallengeResponseGame currentGame=games.get(generator.nextInt(1));
 //add the username to the current game
-currentGame.addPlayer(UserName);                
+currentGame.addPlayer(UserName);
 //Question selection is done by random choice
 //Store the question for the current game by getting the questions
 //then index of the next random number to get the single question
@@ -123,11 +138,11 @@ String question = currentGame.getQuestions().get(gameChoice2).getQuestion();
 //UserAnswer for the question
 String userAnswer = input.readLine();
 //checking the answer for the question of the current game by this.UserName
-currentGame.answer(UserName, question, userAnswer);            
+currentGame.answer(UserName, question, userAnswer);
 }
 void Scoreboard(){//temp methods just for ease of understanding
 int gameChoice3 = input.readLine();
 //Should get the game at the choice of the user and return the names and scores
-games.get(gameChoice).getScores();             
+games.get(gameChoice).getScores();
 }
  */
